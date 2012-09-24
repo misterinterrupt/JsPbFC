834e35c666165a33d476d52f2a6490cba6ff8dc5
Matt Howell <matthew@rootmusic>
2012-09-24 10:46:21 -0700

    adding the start of a future implementation

---
+++ Future.js
@@ -0,0 +1,28 @@
+(function(exports) {
+  
+  var always = function() {
+        
+      },
+      done = function() {
+        
+      },
+      fail = function() {
+        
+      },
+      resolve = function() {
+        
+      },
+      reject = function() {
+        
+      },
+      when = function() {
+        
+      },
+      Future = function() {
+        
+      }
+  module.exports = Future;
+})(exports);
+
+var p = new Future();
+
