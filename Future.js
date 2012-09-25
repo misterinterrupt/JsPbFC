3ea68d6e7065db22aef8396e1041681974a95ea9
Matt Howell <misterinterrupt@gmail.com>
2012-09-25 08:36:58 -0700

    worked on Future stubs and modified README

--- Future.js
+++ Future.js
@@ -1,28 +1,76 @@
 (function(exports) {
   
-  var always = function() {
-        
-      },
-      done = function() {
-        
-      },
-      fail = function() {
-        
-      },
-      resolve = function() {
-        
-      },
-      reject = function() {
-        
-      },
-      when = function() {
-        
-      },
-      Future = function() {
-        
-      }
+  var callbacks = {
+        always: [],
+        done: [],
+        fail: []        
+    },
+    always = function(cb) {
+      callbacks.always.push(cb);
+    },
+    done = function(cb) {
+      callbacks.done.push(cb);
+    },
+    fail = function(cb) {
+      callbacks.fail.push(cb);
+    },
+    resolve = function() {
+      // run the done cbs
+      // run the always cbs
+      // trigger the promise resolution
+    },
+    reject = function() {
+      // run the fail cbs
+      // run the always cbs  
+    };
+  
+  function Promise() {
+    this.always = always;
+    this.done = done;
+    this.fail = fail;
+  }
+  
+  function Future() {
+    this._promise = new Promise();
+    this.promise = function() {
+      return this._promise;
+    };
+    this.always = this._promise.always;
+    this.done = this._promise.done;
+    this.fail = this._promise.fail;
+    this.resolve = resolve;
+    this.reject = reject;
+
+  }
+  
+  Future.when = function(promises) {
+   // return a promise representing multiple promises 
+  }
+  
+  Promise.then = function(operations) {
+    // hook onto the promise resolution
+    // trigger operations upon promise resolution
+  }
+  
   module.exports = Future;
 })(exports);
 
-var p = new Future();
-
+// var p1 = new Future(function() {
+//   console.log('Now is the time.');
+// });
+// 
+// var p2 = new Future(function() {
+//   console.log('It is later than you think.');
+// });
+// 
+// Future.when([p1, p2], function() {
+//   
+// });
+// 
+// setTimeout(1000, function() {
+//   p1.resolve();
+// });
+// 
+// setTimeout(2500, function() {
+//   p2.resolve();
+// });
