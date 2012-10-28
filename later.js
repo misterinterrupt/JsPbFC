09d0f892def2c839883f5c92d285a97b495bf715
Matty <matty@unknown68a86d0ec704.gateway.sonic.net>
2012-10-28 06:06:01 -0700

    trivial changes to the quick and dirty queueing prototype, later.js  Please enter the commit message for your changes. Lines starting

--- later.js
+++ later.js
@@ -3,12 +3,21 @@ function laterDefinition() {
   return {
            later: function(f){ funcs.push(f);},
            now: function() { for(var i=0; i < funcs.length; i++) { funcs[i](); } },
-           procrastinating: function() { return funcs; }
+           procrastinating: function() { /* equivalent of sleep function */ },
+           whatElse: function() { return funcs; }
   };
 }
 
-later.later(function(){console.log('no time like '+ new Date() )});
-later.later(function(){console.log('no time like '+ new Date() )});
-later.later(function(){console.log('no time like '+ new Date() )});
+var testFunc = function(){
+	console.log('no time like ' + new Date());
+};
+
+// simple test
+
+later.later(testFunc);
+later.later(testFunc);
+later.later(testFunc);
+
 later.procrastinating();
+
 later.now();
